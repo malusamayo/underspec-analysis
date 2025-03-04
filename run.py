f28be086006ebcb37df7e7322481c8c741db473b
Chenyang Yang <773386@gmail.com>
2025-03-03 20:48:18 -0500

    update LM configurations and add elicitation mode

--- run.py
+++ run.py
@@ -5,25 +5,27 @@ import dspy
 import litellm
 import mlflow
 from analysis.load_data import prepare_data
-from analysis.utils import use_lm, run_model
+from analysis.utils import run_model
+
+load_dotenv()
 
 lm_dict = {
     "gpt-4o": dspy.LM('openai/gpt-4o-2024-08-06', temperature=1.0),
     "gpt-4o-mini": dspy.LM('openai/gpt-4o-mini-2024-07-18', temperature=1.0),
-    "o3-mini": dspy.LM('openai/o3-mini', temperature=1.0),
-    "gemini-1.5-pro": dspy.LM('openai/gemini-1.5-pro-002', temperature=1.0, api_base = "https://cmu-aiinfra.litellm-prod.ai/", api_key = os.environ.get("LITELLM_API_KEY")),
-    "llama3-2-11b-instruct": dspy.LM('openai/llama3-2-11b-instruct', temperature=1.0, api_base = "https://cmu-aiinfra.litellm-prod.ai/", api_key = os.environ.get("LITELLM_API_KEY")),
+    "o3-mini": dspy.LM('openai/o3-mini', temperature=1.0, max_tokens=10000),
+    "gemini-1.5-pro": dspy.LM('openai/gemini-1.5-pro-002', temperature=1.0, api_base=os.environ.get("CMU_API_BASE"), api_key=os.environ.get("LITELLM_API_KEY")),
+    "llama3-2-11b-instruct": dspy.LM('openai/llama3-2-11b-instruct', temperature=1.0, api_base=os.environ.get("CMU_API_BASE"), api_key=os.environ.get("LITELLM_API_KEY")),
     "mixtral-8x7b": dspy.LM('bedrock/mistral.mixtral-8x7b-instruct-v0:1', temperature=1.0),
+    "qwen2.5-7b": dspy.LM('hosted_vllm/Qwen/Qwen2.5-7B-Instruct', temperature=1.0, api_base=os.environ.get("BABEL_API_BASE")),
+    "ministral-8b": dspy.LM('hosted_vllm/mistralai/Ministral-8B-Instruct-2410', temperature=1.0, api_base=os.environ.get("BABEL_API_BASE")),
+    "llama3.1-8b": dspy.LM('hosted_vllm/meta-llama/Llama-3.1-8B-Instruct', temperature=1.0, api_base=os.environ.get("BABEL_API_BASE")),
 }
-
-lm_dict['o3-mini'].kwargs['max_completion_tokens'] = lm_dict['o3-mini'].kwargs.pop('max_tokens')
 
 if __name__ == "__main__":
 
-    load_dotenv()
-
     parser = argparse.ArgumentParser()
     parser.add_argument("--experiment", type=str, help="The name of the experiment to log to.")
+    parser.add_argument("--elicitation_mode", action='store_true', help="Whether to run in elicitation mode.")
     args = parser.parse_args()
 
     if args.experiment:
