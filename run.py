b4e97baaefd471dd85bff0689a10e2cec0526f43
Chenyang Yang <773386@gmail.com>
2025-03-13 21:55:53 -0400

    update model settings

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
+import pandas as pd
+import numpy as np
+import json
 import argparse
 import os
 from dotenv import load_dotenv
@@ -5,7 +8,7 @@ import dspy
 import litellm
 import mlflow
 from analysis.load_data import prepare_data
-from analysis.utils import run_model
+from analysis.utils import run_model, requirements_to_str
 
 load_dotenv()
 
@@ -14,11 +17,12 @@ lm_dict = {
     "gpt-4o-mini": dspy.LM('openai/gpt-4o-mini-2024-07-18', temperature=1.0),
     "o3-mini": dspy.LM('openai/o3-mini', temperature=1.0, max_tokens=10000),
     "gemini-1.5-pro": dspy.LM('openai/gemini-1.5-pro-002', temperature=1.0, api_base=os.environ.get("CMU_API_BASE"), api_key=os.environ.get("LITELLM_API_KEY")),
+    "claude-3.5-sonnet": dspy.LM('openai/claude-3-5-sonnet-20241022', temperature=1.0, api_base=os.environ.get("CMU_API_BASE"), api_key=os.environ.get("LITELLM_API_KEY")),
     "llama3-2-11b-instruct": dspy.LM('openai/llama3-2-11b-instruct', temperature=1.0, api_base=os.environ.get("CMU_API_BASE"), api_key=os.environ.get("LITELLM_API_KEY")),
     "mixtral-8x7b": dspy.LM('bedrock/mistral.mixtral-8x7b-instruct-v0:1', temperature=1.0),
-    "qwen2.5-7b": dspy.LM('hosted_vllm/Qwen/Qwen2.5-7B-Instruct', temperature=1.0, api_base=os.environ.get("BABEL_API_BASE")),
-    "ministral-8b": dspy.LM('hosted_vllm/mistralai/Ministral-8B-Instruct-2410', temperature=1.0, api_base=os.environ.get("BABEL_API_BASE")),
-    "llama3.1-8b": dspy.LM('hosted_vllm/meta-llama/Llama-3.1-8B-Instruct', temperature=1.0, api_base=os.environ.get("BABEL_API_BASE")),
+    "qwen2.5-7b": dspy.LM('hosted_vllm/Qwen/Qwen2.5-7B-Instruct', temperature=0.7, api_base=os.environ.get("BABEL_API_BASE")),
+    "ministral-8b": dspy.LM('hosted_vllm/mistralai/Ministral-8B-Instruct-2410', temperature=0, api_base=os.environ.get("BABEL_API_BASE")),
+    "llama3.1-8b": dspy.LM('hosted_vllm/meta-llama/Llama-3.1-8B-Instruct', temperature=0.6, api_base=os.environ.get("BABEL_API_BASE")),
 }
 
 if __name__ == "__main__":
