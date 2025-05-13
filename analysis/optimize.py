32be1d02bc22d7cdc7f210e30bcce5a4756902cc
Chenyang Yang <773386@gmail.com>
2025-05-12 22:26:35 -0400

    refactor optimize.py to enhance optimizer construction and streamline prompt optimization; add YAML config support

--- analysis/optimize.py
+++ analysis/optimize.py
@@ -2,6 +2,9 @@ import pandas as pd
 import numpy as np
 import json
 import logging
+import re
+import argparse
+import yaml
 
 from .optimizers.copro import COPRO
 from .optimizers.textgrad import TextGradOptimizer
@@ -54,101 +57,115 @@ def gather_relevant_requirements(prompt_name, requirements):
     if prompt_name.startswith("fixed"):
         indices = prompt_name.split("_")[1].split("+")
         indices = [int(i) for i in indices]
-        relevant_requirements = [req for i, req in enumerate(requirements) if i in indices]
+        relevant_requirements = [requirements[i] for i in indices]
         return relevant_requirements
     elif prompt_name.startswith("task_only"):
         return []
-    raise ValueError(f"Unknown prompt name: {prompt_name}")
+    return []
 
-if __name__ == "__main__":
-
-    task = "trip"
-    task_description, TaskProgram, trainset, valset, requirements, prompts = prepare_data(
-        task_name=task,
-        configs={
-            "prompt_path": f"data/prompts/trip_n10.json",
-        }
-    )
-    task_program = TaskProgram(lm=LM_DICT["gpt-4o"])
-
-    
-    judge = LLMJudge(task_description=task_description, lm=LM_DICT["4.1-mini-eval"], max_workers=64, omit_input=False)
-
-    prompt_subsets = [
-        "fixed",
-    ]
-
-    key_in_subset = lambda key: any([key.startswith(subset) for subset in prompt_subsets])
-
-    prompts = {
-        k: v for k, v in prompts.items() if key_in_subset(k)
-    }
-    optimized_prompts = {}
-    for i, (prompt_name, prompt) in enumerate(prompts.items()):
-        print(f"Optimizing {prompt_name}...")
-        # if prompt_name != "fixed_0+1+2+3+4+5+6+7+8+9":
-        #     continue
-        # if i <= 5:
-        #     continue
-        local_requirements = gather_relevant_requirements(prompt_name, requirements)
-
-        task_program.prompt = prompt
-        
-        # optimizer = COPRO(
-        #     prompt_model=LM_DICT["gpt-4o"],
-        #     evaluate=evaluate_requirements(requirements),
-        #     breadth=3,
-        #     depth=3,
-        #     init_temperature=0.5,
-        # )    
+def construct_optimizer(
+    optimizer_name,
+    task_description,
+    local_requirements,
+):
+    if optimizer_name.endswith("textgrad"):
         optimizer = TextGradOptimizer(
             evaluate=evaluate_requirements(local_requirements),
             prompt_model=LM_DICT["gpt-4o"],
             num_epochs=3,
             batch_size=10,
         )
-        optimizer_name = "textgrad"
+    elif optimizer_name == "openai":
+        optimizer = OpenAIPromptOptimizer(seed=42)
+    elif optimizer_name == "reqaware":
+        optimizer = ReqAwareOptimizer(
+            task_description=task_description,
+            requirements=[requirement["requirement"] for requirement in local_requirements],
+            evaluate=evaluate_requirements(local_requirements),
+            num_trials=9,
+            seed=42
+        )
+    # elif optimizer_name == "textgrad_reqaware":
+    #     # first find the prompt with textgrad
+    #     textgrad_prompt = prompts_copy[f"optimized_{model_name}_textgrad_" + prompt_name]
+    #     intro_match = re.search(r"^.*?(?=\n-)", textgrad_prompt, re.DOTALL)
+    #     task_description = intro_match.group(0).strip() if intro_match else None
+    #     requirement_components = re.findall(r"^- (.+)", textgrad_prompt, re.MULTILINE)
 
-        # optimizer = OpenAIPromptOptimizer(seed=42)
-        # optimizer_name = "openai"
+    #     # then find a good combination
+    #     optimizer = ReqAwareOptimizer(
+    #         task_description=task_description,
+    #         requirements=requirement_components,
+    #         evaluate=evaluate_requirements(local_requirements),
+    #         num_trials=9,
+    #         seed=42
+    #     )
+    elif optimizer_name == "copro":
+        optimizer = COPRO(
+            prompt_model=LM_DICT["gpt-4o"],
+            evaluate=evaluate_requirements(local_requirements),
+            breadth=3,
+            depth=3,
+            init_temperature=0.5,
+        )
+    return optimizer
 
-        compiled_task_module = optimizer.compile(task_program, trainset[:30])
+if __name__ == "__main__":
 
-        optimized_prompts["optimized_" + optimizer_name + "_" + prompt_name] = compiled_task_module.prompt
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", type=str, help="The path to the config file.", required=True)
+    args = parser.parse_args()
+
+    with open(args.config, "r") as f:
+        config = yaml.safe_load(f)
+
+    task = config["task_name"]
+    task_description, TaskProgram, trainset, valset, requirements, prompts = prepare_data(
+        task_name=task,
+        configs={
+            "prompt_paths": config["prompt_paths"],
+        }
+    )
+    model_name = config["model_name"]
+    task_program = TaskProgram(lm=LM_DICT[model_name])
+
+    optimizer_names = config["optimizer_names"]
     
-        print(json.dumps(optimized_prompts, indent=4))
+    judge = LLMJudge(task_description=task_description, lm=LM_DICT["4.1-mini-eval"], max_workers=64, omit_input=False)
 
-    # compiled_task_module = optimizer.compile(task_program, trainset=trainset[:30])
-    # print(compiled_task_module.prompt)
-    # evaluate(compiled_task_module, trainset[:30])
-    # evaluate(compiled_task_module, trainset[30:])
+    prompt_subsets = config["prompt_subsets"]
 
+    key_in_subset = lambda key: any([key.startswith(subset) for subset in prompt_subsets])
 
-    # compiled_task_module = optimizer.compile(task_program, trainset[:30])
-    # all_prompts = {
-    #     "optimized_" + str(k-1): v['prompt']
-    #     for k, v in compiled_task_module.prompt_history.items() if k > 0
-    # }
-    # print(json.dumps(compiled_task_module.prompt_history))
-    # print(json.dumps(all_prompts, indent=4))
+    prompts_copy = prompts.copy()
+    prompts = {
+        k: v for k, v in prompts.items() if key_in_subset(k)
+    }
+    optimized_prompts = {}
+    for i, (prompt_name, prompt) in list(enumerate(prompts.items()))[3:]:
+        for optimizer_name in optimizer_names:
+            print(f"Optimizing {prompt_name} with {optimizer_name}...")
+            
+            local_requirements = gather_relevant_requirements(prompt_name, requirements)
 
-    # optimizer = MIPROv2(
-    #     evaluate=evaluate,
-    #     prompt_model=LM_DICT["gpt-4o"],
-    #     auto="light"
-    # )
-    # compiled_task_module = optimizer.compile(task_program, trainset=trainset[:30])
+            task_program.prompt = prompt
 
-    # optimizer = ReqAwareOptimizer(
-    #     task_description=task_description,
-    #     requirements=[requirement["requirement"] for requirement in requirements],
-    #     evaluate=evaluate,
-    #     num_trials=10,
-    #     seed=42
-    # )
-    # compiled_task_module = optimizer.compile(task_program, trainset[:30])
+            if optimizer_name == "reqaware+textgrad":
+                task_program.prompt = prompts_copy[f"optimized_{model_name}_reqaware_" + prompt_name]
 
-    # print(json.dumps({
-    #     "optimized": compiled_task_module.prompt,
-    # }, indent=4))
+            optimizer = construct_optimizer(
+                optimizer_name=optimizer_name,
+                task_description=task_description,
+                local_requirements=local_requirements,
+            )
+
+            compiled_task_module = optimizer.compile(task_program, trainset[:30])
+
+            optimized_prompts["optimized_" + model_name + "_" + optimizer_name + "_" + prompt_name] = compiled_task_module.prompt
+
+            # print(json.dumps(optimized_prompts, indent=4))
+
+    output_path = config["prompt_paths"][0].replace(".json", "_optimized.json")
+    with open(output_path, "w") as f:
+        f.write(json.dumps(optimized_prompts, indent=4))
